16ebc5bdb160dddd408527f64c4806d4158c2456
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-01 08:29:24 +0200

    google authentication

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import { GoogleLogin } from "@react-oauth/google";
 
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
