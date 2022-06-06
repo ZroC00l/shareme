5e8bf31f60c46783f7cab1e8cdef8f28ca378cab
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-06 12:39:13 +0200

    togglesidebar for mobile devices/ desktop devices

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 import { HiMenu } from "react-icons/hi";
-import { AiCloseFillCirlse } from "react-icons/ai";
+import { AiFillCloseCircle } from "react-icons/ai";
 import { Route, Routes, Link } from "react-router-dom";
 
 import { Sidebar, UserProfile } from "../components";
@@ -15,6 +15,7 @@ import { userQuery } from "../utils/data";
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
+  const scrollRef = useRef(null);
 
   //Pull user from local storage
   const userInfo =
@@ -31,16 +32,21 @@ const Home = () => {
     });
   }, []);
 
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out ">
       <div className="hidden md:flex h-screen flex-initial ">
-        <Sidebar />
+        {/* Mobile Phone Sidebar */}
+        <Sidebar user={user && user} />
       </div>
       <div className="flex md:hidden flex-row ">
         <HiMenu
           fontSize={40}
           className="cursor-pointer"
-          onClick={() => setToggleSidebar(false)}
+          onClick={() => setToggleSidebar(true)}
         />
         <Link to="/">
           <img src={logo} alt="logo" className="w-28" />
@@ -49,6 +55,23 @@ const Home = () => {
           <img src={user?.image} alt="logged-In-user-dp" className="w-28" />
         </Link>
       </div>
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle
+              fontSize={30}
+              className="cursor-pointer"
+              onClick={() => setToggleSidebar(false)}
+            />
+          </div>
+          {/* Desktop Devices Sidebar */}
+          <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+        </div>
+      )}
+      <div
+        className="pb-2 flex-1 h-screen overflow-y-scroll "
+        ref={scrollRef}
+      ></div>
     </div>
   );
 };
