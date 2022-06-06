e0dffa26aaef1dd7317eb877edaccceaa34040a3
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-06 17:04:43 +0200

    Home page component update

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -43,35 +43,40 @@ const Home = () => {
         <Sidebar user={user && user} />
       </div>
       <div className="flex md:hidden flex-row ">
-        <HiMenu
-          fontSize={40}
-          className="cursor-pointer"
-          onClick={() => setToggleSidebar(true)}
-        />
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28" />
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="logged-In-user-dp" className="w-28" />
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-          <div className="absolute w-full flex justify-end items-center p-2">
-            <AiFillCloseCircle
-              fontSize={30}
-              className="cursor-pointer"
-              onClick={() => setToggleSidebar(false)}
-            />
-          </div>
-          {/* Desktop Devices Sidebar */}
-          <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="logged-In-user-dp" className="w-28" />
+          </Link>
         </div>
-      )}
-      <div
-        className="pb-2 flex-1 h-screen overflow-y-scroll "
-        ref={scrollRef}
-      ></div>
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <AiFillCloseCircle
+                fontSize={30}
+                className="cursor-pointer"
+                onClick={() => setToggleSidebar(false)}
+              />
+            </div>
+            {/* Desktop Devices Sidebar */}
+            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll " ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
     </div>
   );
 };
