d5e55cdfbcfa3819c4a8bd91927474f7115e7d8d
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-06 07:07:57 +0200

    Home page component

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -5,9 +5,28 @@ import { AiCloseFillCirlse } from "react-icons/ai";
 import { Route, Routes, Link } from "react-router-dom";
 
 import { Sidebar, UserProfile } from "../components";
+import Pins from "./Pins";
+
+import { client } from "../client";
+import logo from "../assets/logo.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [toggleSidebar, setToggleSidebar] = useState();
+
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out ">
+      <div className="hidden md:flex h-screen flex-initial ">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row ">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(false)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
