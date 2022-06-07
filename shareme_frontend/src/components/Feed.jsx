e0bf8b3e612e064cc92b34b6953aaf6912780d1a
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-07 09:40:20 +0200

    MansoryLayout/Spinner component

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
 
 const Feed = () => {
   return <div>Feed</div>;
---
+++ shareme_frontend/src/components/MansoryLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MansoryLayout = () => {
+  return <div>MansoryLayout</div>;
+};
+
+export default MansoryLayout;
