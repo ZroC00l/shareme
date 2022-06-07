0f7392e221b5ea75886cae2224950d6d0fd0ddc9
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-07 09:24:30 +0200

    Navbar component update

--- shareme_frontend/src/container/Pins.jsx
+++ shareme_frontend/src/container/Pins.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navbar, CreatePin, Search, Feed, PinDetail } from "../components";
 
-const Pins = () => {
+const Pins = ({ user }) => {
   /*This state is placed in Pins and not Search component, because it will be shared 
   across mutiple components used by Pins component*/
   const [searchTerm, setSearchTerm] = useState("");
@@ -11,7 +11,28 @@ const Pins = () => {
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
-        <Navbar searchTerm={searchTerm} />
+        <Navbar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user} />}
+          />
+          <Route path="/createPin" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
       </div>
     </div>
   );
