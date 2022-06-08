88d7533c9dec2d96320a972b1889939459c92297
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-08 07:17:07 +0200

    Pin image hover state change

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { MdDownloadForOffline } from "react-icons/md";
+import { AiTwoToneDelete } from "react-icons/ai";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+
+import { client } from "../client";
 import { urlFor } from "../client";
 
-const Pin = ({ pin: { postedBy, image } }) => {
+const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+  const navigate = useNavigate();
+
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+
   return (
-    <div>
+    <div className="m-2">
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(true)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <div className=""></div>
+      </div>
+
       <img
         className="rounded-lg w-full"
         alt="user-post"
-        src={urlFor(image).width(250).url()}
+        src={urlFor(image).width(200).url()}
       />
     </div>
   );
