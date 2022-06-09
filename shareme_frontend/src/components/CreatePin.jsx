58bcb6fc6721672ad3661bc42f1a1f95b270805a
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-09 08:22:17 +0200

    createPin states

--- shareme_frontend/src/components/CreatePin.jsx
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -5,8 +5,19 @@ import { useNavigate } from "react-router-dom";
 
 import { client } from "../client";
 import Spinner from "./Spinner";
+import { categories } from "../utils/data";
 
 const CreatePin = ({ user }) => {
+  // state fields
+  const [title, setTitle] = useState("");
+  const [about, setAbout] = useState("");
+  const [destination, setDestination] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImagetype, setWrongImagetype] = useState(false);
+
   return <div>CreatePin</div>;
 };
 
