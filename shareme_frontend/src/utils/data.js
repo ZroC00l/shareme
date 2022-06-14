32313d37604c55e653166197755cd568a46f0aa8
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-14 13:06:34 +0200

    removed some unnecessary static data

--- shareme_frontend/src/utils/data.js
+++ shareme_frontend/src/utils/data.js
@@ -14,24 +14,9 @@ export const categories = [
       "https://i.pinimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg",
   },
   {
-    name: "wallpaper",
-    image:
-      "https://i.pinimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg",
-  },
-  {
-    name: "websites",
-    image:
-      "https://i.pinimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg",
-  },
-  {
-    name: "photo",
+    name: "photography",
     image:
       "https://i.pinimg.com/236x/72/8c/b4/728cb43f48ca762a75da645c121e5c57.jpg",
-  },
-  {
-    name: "food",
-    image:
-      "https://i.pinimg.com/236x/7d/ef/15/7def15ac734837346dac01fad598fc87.jpg",
   },
 
   {
@@ -39,21 +24,13 @@ export const categories = [
     image:
       "https://i.pinimg.com/736x/f4/e5/ba/f4e5ba22311039662dd253be33bf5f0e.jpg",
   },
-  {
-    name: "travel",
-    image:
-      "https://i.pinimg.com/236x/fa/95/98/fa95986f2c408098531ca7cc78aee3a4.jpg",
-  },
+
   {
     name: "quotes",
     image:
       "https://i.pinimg.com/236x/46/7c/17/467c17277badb00b638f8ec4da89a358.jpg",
   },
-  {
-    name: "cats",
-    image:
-      "https://i.pinimg.com/236x/6c/3c/52/6c3c529e8dadc7cffc4fddedd4caabe1.jpg",
-  },
+
   {
     name: "dogs",
     image:
